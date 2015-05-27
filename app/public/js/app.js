be4a82682bd3e4351a84f19af027bde39873abd4
Anace313 <anas.ieee@gmail.com>
2015-05-27 13:08:20 +0200

    Defining client side app and routes

---
+++ app/public/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('adsHandlerApp', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/views/ads.html',
+                controller: 'adsController'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: '/views/about.html'
+            })
+            .state('edit', {
+                url: '/edit/{id}',
+                templateUrl: '/views/editAd.html',
+                controller: 'editAdController'
+            });
+    });
